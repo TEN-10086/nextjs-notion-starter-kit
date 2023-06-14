1cbec016b0332afa1ada618bb2a885d112e5cdfe
TEN-10086 <136579788+TEN-10086@users.noreply.github.com>
2023-06-14 17:26:13 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,23 +2,23 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '84270ba9c5cf4c9b9c7fa09be29c8529',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'POP趨勢-線上培訓平台',
+  domain: 'https://www.notion.so/406f066099de4ad4a6f89a28602b6485?v=84270ba9c5cf4c9b9c7fa09be29c8529',
+  author: 'TEN & HAN',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
+  github: 'TEN-10086',
   linkedin: 'fisch2',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
